f8a4ad377735b3495366113d20b32106b4d9139d
Laurajcb <callejaslaura96@gmail.com>
2023-01-11 10:42:27 -0500

    Added logic to get textfiel data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Container from '@mui/material/Container';
+import { Searcher } from "./Components/Searcher";
 
 const App = () => {
-  return(
-    <div>Soy un platzinauta</div>
+  const [inputUser, setInputUser] = useState('Octocat');
+  const [userState, setUserState] = useState('inputUser');
+  console.log('input user', inputUser);
+
+
+  return (
+    <Container sx={{
+      background: 'whitesmoke',
+      width: '80vw',
+      height: '500px',
+      borderRadius: '16px',
+      marginTop: '40px',
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}>
+      <Searcher inputUser={inputUser} setInputUser={setInputUser}/>
+    </Container>
   )
 };
 
