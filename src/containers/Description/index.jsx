e3e639148a4ffec13a9c3dff0d062ab005c4205d
Laurajcb <callejaslaura96@gmail.com>
2023-01-12 12:49:55 -0500

    Added styles to organize components

--- src/containers/Description/index.jsx
+++ src/containers/Description/index.jsx
@@ -8,10 +8,10 @@ const Description = (props) => {
   const { bio } = userState
   return (
     <>
-      <Stack>
+      <Stack sx={{justifyContent:'center'}}>
         {bio != null
-          ? <Typography>{bio}</Typography>
-          : <Typography>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
+          ? <Typography variant='body1'>{bio}</Typography>
+          : <Typography variant='body1'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Typography>
         }
       </Stack>
       <PaperInfo  userState={userState}/>
--- src/containers/userCard/index.jsx
+++ src/containers/userCard/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CardMedia, Grid } from "@mui/material";
 import { MainInformation } from "../../Components/MainInformation";
 import { Description } from "../Description";
+import { Stack } from "@mui/system";
 
 const UserCard = (props) => {
   const { userState } = props;
@@ -10,18 +11,34 @@ const UserCard = (props) => {
   return (
     <Grid
       container
+      spacing={2}
+      sx={{
+        marginTop: '15px'
+      }}
     >
       <Grid item xs={3}>
         <CardMedia
           component='img'
           alt="GitHub User"
           image={avatar_url}
+          sx={{
+            borderRadius: '50%',
+            marginLeft: '5px'
+          }}
         />
       </Grid>
       <Grid item xs={9}>
-        <MainInformation userState={userState} />
+        <Stack 
+        direction="column"
+        spacing={1}
+        sx={{
+          margin:'30px'
+        }}
+        >
+          <MainInformation userState={userState} />
+          <Description userState={userState} />
+        </Stack>
       </Grid>
-        <Description userState={userState}/>
     </Grid>
   )
 }
